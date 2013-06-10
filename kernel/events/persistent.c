f8d8bcfd122c9ef708bbe237aa874d57b151a5f8
Robert Richter <robert.richter@linaro.org>
2013-06-10 16:31:05 +0200

    perf, persistent: Implementing a persistent pmu
    We want to use the kernel's pmu design to later expose persistent
    events via sysfs to userland. Initially implement a persistent pmu.
    
    The format syntax is introduced allowing to set bits anywhere in
    struct perf_event_attr. This is used in this case to set the
    persistent flag (attr5:23). The syntax is attr<num> where num is the
    index of the u64 array in struct perf_event_attr. Otherwise syntax is
    same as for config<num>.
    
    Patches that implement this functionality for perf tools are sent in a
    separate patchset.
    
    Signed-off-by: Robert Richter <robert.richter@linaro.org>
    Signed-off-by: Robert Richter <rric@kernel.org>

--- kernel/events/persistent.c
+++ kernel/events/persistent.c
@@ -175,9 +175,44 @@ out:
 	return event_fd;
 }
 
+PMU_FORMAT_ATTR(persistent, "attr5:23");
+
+static struct attribute *persistent_format_attrs[] = {
+	&format_attr_persistent.attr,
+	NULL,
+};
+
+static struct attribute_group persistent_format_group = {
+	.name = "format",
+	.attrs = persistent_format_attrs,
+};
+
+static const struct attribute_group *persistent_attr_groups[] = {
+	&persistent_format_group,
+	NULL,
+};
+
+static struct pmu persistent_pmu;
+
+static int persistent_pmu_init(struct perf_event *event)
+{
+	if (persistent_pmu.type != event->attr.type)
+		return -ENOENT;
+
+	/* Not a persistent event. */
+	return -EFAULT;
+}
+
+static struct pmu persistent_pmu = {
+	.event_init	= persistent_pmu_init,
+	.attr_groups	= persistent_attr_groups,
+};
+
 void __init persistent_events_init(void)
 {
 	int cpu;
+
+	perf_pmu_register(&persistent_pmu, "persistent", -1);
 
 	for_each_possible_cpu(cpu) {
 		INIT_LIST_HEAD(&per_cpu(pers_events, cpu));
